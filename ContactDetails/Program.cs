ce6f5268ae80bf03d3d5ae73f5830cc10f75567f
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-20 17:05:52 +0530

    [Biplab] Add: Contact details of person

---
+++ ContactDetails/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ContactDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                ContactAddress contactAddress = new ContactAddress();
+
+                contactAddress.GetContact();
+        }
+        
+    }
+}
